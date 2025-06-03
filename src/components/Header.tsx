ea134173022c0c2cdeea4eee98272373756e4859
Aristoteles Alves <aristotelesalves39@gmail.com>
2025-06-03 17:05:40 -0300

    fix: home page

---
+++ src/components/Header.tsx
@@ -0,0 +1,38 @@
+export function Header() {
+    return (
+        <header className="w-full sticky top-4 left-0 flex items-center justify-center z-50">
+            <ul className="flex px-10 py-4 bg-black/50 backdrop-blur-md border-2 rounded-2xl border-[#303030] text-white gap-10">
+                <li className="h-fit w-fit relative flex flex-col group">
+                    <a className="px-4 py-0" href="#home">
+                        Inicio
+                    </a>
+                    <div className="h-[1px] rounded-full shadow-md shadow-green-400 w-0 bg-green-300 group-hover:w-full transition-all duration-200"></div>
+                </li>
+                <li className="h-fit w-fit relative flex flex-col group">
+                    <a className="px-4 py-0" href="#about">
+                        Sobre
+                    </a>
+                    <div className="h-[1px] rounded-full shadow-md shadow-green-400 w-0 bg-green-300 group-hover:w-full transition-all duration-200"></div>
+                </li>
+                <li className="h-fit w-fit relative flex flex-col group">
+                    <a className="px-4 py-0" href="#skills">
+                        Habilidades
+                    </a>
+                    <div className="h-[1px] rounded-full shadow-md shadow-green-400 w-0 bg-green-300 group-hover:w-full transition-all duration-200"></div>
+                </li>
+                <li className="h-fit w-fit relative flex flex-col group">
+                    <a className="px-4 py-0" href="#projects">
+                        Projetos
+                    </a>
+                    <div className="h-[1px] rounded-full shadow-md shadow-green-400 w-0 bg-green-300 group-hover:w-full transition-all duration-200"></div>
+                </li>
+                <li className="h-fit w-fit relative flex flex-col group">
+                    <a className="px-4 py-0" href="#contact">
+                        Contato
+                    </a>
+                    <div className="h-[1px] rounded-full shadow-md shadow-green-400 w-0 bg-green-300 group-hover:w-full transition-all duration-200"></div>
+                </li>
+            </ul>
+        </header>
+    )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,138 +1,80 @@
 import { Cards, GithubLogo, InstagramLogo, LinkedinLogo, WhatsappLogo } from "phosphor-react";
+import { Header } from "../components/Header";
+import { ArrowDown } from "lucide-react";
 
 export function Home() {
   return (
-    <body className="w-full bg-black relative flex flex-col items-center justify-center scroll-smooth">
-      <div className="w-full sticky top-4 left-0 flex items-center justify-center z-50">
-        <ul className="flex px-10 py-4 bg-black border-zinc-800 border-2 rounded-2xl text-white gap-10">
-          <li>
-            <a className="px-4 py-2 hover:bg-zinc-800 rounded-full" href="#">
-              Inicio
+    <body className="w-full  bg-black relative flex flex-col items-center justify-center ">
+      <Header />
+      <section id="home" className="h-screen w-full relative flex flex-col">
+        <section className="h-full flex flex-col items-center justify-center pb-20 gap-4 z-40  rounded-full">
+          <span className="px-20 py-2 bg-black border-2 border-[#CCCCCC] shadow-md rounded-3xl mb-2 text-[#CCCCCC] z-40">
+            Desenvolvimento Web
+          </span>
+          <div className="z-40">
+            <h1 className="text-center text-[#CCCCCC] text-4xl sm:text-5xl md:text-6xl font-bold leading-tight px-2">
+              Transformo ideias em
+            </h1>
+            <h1 className="text-center text-[#CCCCCC] text-4xl sm:text-5xl md:text-6xl font-bold leading-tight px-2">
+              experiências digitais incríveis
+            </h1>
+          </div>
+          <p className="text-center text-[#A6A6A6] text-sm sm:text-base md:text-lg max-w-[90vw] sm:max-w-xl mx-auto mt-3 px-2 z-40">
+            Olá, meu nome é Aristóteles Alves e sou desenvolvedor Full Stack.
+            Explore a página para saber mais sobre minha trajetória e conhecer projetos que tornam ideias realidade.
+          </p>
+          <div className="flex gap-4 mt-8 z-40">
+            <a
+              href="#about"
+              className="px-10 py-3 bg-black flex items-center justify-center text-[#CCCCCC] hover:text-white hover:border-green-600 border-2 rounded-2xl border-[#303030]  transition-colors gap-2"
+            >
+              <span>👋</span>
+              <p>Quem sou eu</p>
             </a>
-          </li>
-          <li>
-            <a className="px-4 py-2 hover:bg-zinc-800 rounded-full" href="#">
-              Sobre
+            <a
+              href="#projects"
+              className="px-10 py-3 bg-black flex items-center justify-center text-[#CCCCCC] hover:text-white hover:border-green-600 border-2 rounded-2xl border-[#303030]  transition-colors gap-2"
+            >
+              <p>Ver Projetos</p>
+              <ArrowDown size={20} />
             </a>
-
-          </li>
-          <li>
-            <a className="px-4 py-2 hover:bg-zinc-800 rounded-full" href="#">
-              Habilidades
-            </a>
-
-          </li>
-          <li>
-            <a className="px-4 py-2 hover:bg-zinc-800 rounded-full" href="#">
-              Projetos
-            </a>
-
-          </li>
-        </ul>
-      </div>
-      <div id="#sobre" className="h-screen w-full relative flex flex-col">
-        <section className="h-full flex flex-col items-center justify-center pb-20">
-          <h1 className="text-center text-white text-[60px] w-[700px] font-mono">
-            Criando experiências web bonitas e funcionais
-            Olá, sou Avinash Singh, um desenvolvedor full stack apaixonado por criar aplicativos de alto desempenho, fáceis de usar e escaláveis.
-          </h1>
+          </div>
         </section>
         <img
           className="w-full h-full object-cover absolute left-0 top-0 z-0"
           src="/grid.svg"
           alt=""
         />
-      </div>
+      </section>
 
-      <div id="projetos" className="w-full pt-20">
-        <div className="flex flex-col justify-center items-center gap-20">
-          <div className="w-full text-center">
-            <h1 className="text-4xl font-semibold text-white">Projetos</h1>
-            <p className="text-gray-400 ">
-              Aqui você encontrará uma seleção de projetos nos quais trabalhei e
-              participei.
-            </p>
-          </div>
-          <div className="flex w-full flex-wrap gap-2 justify-center">
-            <div className="text-white bg-black p-1 rounded-xl overflow-hidden flex flex-col items-center justify-center">
-              <div className="w-80 h-80 rounded-lg relative group overflow-hidden">
-                <img
-                  className="z-0"
-                  src="https://i.pinimg.com/originals/97/35/82/973582d9b0e0761a1b880edb78b7f4e7.gif"
-                  alt=""
-                />
-              </div>
+      <section id="about" className="h-screen w-full">
+        <div className="">
+          <h1>
 
-              <div className="p-2">
-                <h2>Nome do projeto</h2>
-              </div>
-            </div>
-            <div className="text-white bg-black p-1 rounded-xl overflow-hidden flex flex-col items-center justify-center">
-              <div className="w-80 h-80 rounded-lg relative group overflow-hidden">
-                <img
-                  className="z-0"
-                  src="https://i.pinimg.com/originals/97/35/82/973582d9b0e0761a1b880edb78b7f4e7.gif"
-                  alt=""
-                />
-              </div>
-
-              <div className="p-2">
-                <h2>Nome do projeto</h2>
-              </div>
-            </div>
-            <div className="text-white bg-black p-1 rounded-xl overflow-hidden flex flex-col items-center justify-center">
-              <div className="w-80 h-80 rounded-lg relative group overflow-hidden">
-                <img
-                  className="z-0"
-                  src="https://i.pinimg.com/originals/97/35/82/973582d9b0e0761a1b880edb78b7f4e7.gif"
-                  alt=""
-                />
-              </div>
-
-              <div className="p-2">
-                <h2>Nome do projeto</h2>
-              </div>
-            </div>
-            <div className="text-white bg-black p-1 rounded-xl overflow-hidden flex flex-col items-center justify-center">
-              <div className="w-80 h-80 rounded-lg relative group overflow-hidden">
-                <img
-                  className="z-0"
-                  src="https://i.pinimg.com/originals/97/35/82/973582d9b0e0761a1b880edb78b7f4e7.gif"
-                  alt=""
-                />
-              </div>
-
-              <div className="p-2">
-                <h2>Nome do projeto</h2>
-              </div>
-            </div>
-          </div>
+          </h1>
         </div>
-      </div>
-      <div id="contato"
-        className="h-full w-full text-white flex items-center justify-center flex-col gap-3 py-10">
-        <div className="w-full text-center">
-          <h1 className="text-4xl font-semibold text-white">Entre em contato</h1>
-          <p className="text-gray-400 ">
-            Algumas das minhas redes sociais
-          </p>
-        </div>
-        <div className="flex gap-2 ">
-          <a className="p-2 rounded-xl hover:bg-green-600 transition-colors duration-200" href="https://api.whatsapp.com/send?phone=5587981152947&text=Olá, tudo bem ? ">
-            <WhatsappLogo size={30} />
-          </a>
-          <a className="p-2 rounded-xl hover:bg-green-600 transition-colors duration-200" href="https://www.instagram.com/aristoteles269/">
-            <InstagramLogo size={30} />
-          </a>
-          <a className="p-2 rounded-xl hover:bg-green-600 transition-colors duration-200" href="https://www.linkedin.com/in/aristoteles-alves-ab8089226/">
-            <LinkedinLogo size={30} />
-          </a>
-          <a className="p-2 rounded-xl hover:bg-green-600 transition-colors duration-200" href="https://github.com/AristotelesAlves">
-            <GithubLogo size={30} />
-          </a>
-        </div>
+        {/* image */}
+        <div>
 
-      </div>
+        </div>
+        <div>
+
+        </div>
+      </section>
+
+      <section id="skills" className="h-screen w-full">
+
+      </section>
+
+      <section id="projects" className="h-screen w-full">
+
+      </section>
+
+      <section id="contact" className="h-screen w-full">
+
+      </section>
+
+
     </body>
   );
 }
