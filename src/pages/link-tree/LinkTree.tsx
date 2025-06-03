6630f22b347a2ea3f27130279193b884dfbe28dc
Aristoteles Alves <aristotelesalves39@gmail.com>
2025-06-02 22:26:57 -0300

    fix: correction link github and instagram

--- src/pages/link-tree/LinkTree.tsx
+++ src/pages/link-tree/LinkTree.tsx
@@ -1,4 +1,4 @@
-import { BriefcaseBusinessIcon, Instagram, Linkedin, Mail, Phone, } from "lucide-react";
+import { BriefcaseBusinessIcon, GithubIcon, Instagram, Linkedin, Mail, Phone, } from "lucide-react";
 import { linktree, getLanguage } from "../../translations/linktree";
 
 export default function LinkTree() {
@@ -6,7 +6,7 @@ export default function LinkTree() {
     const t = linktree[lang];
 
     return (
-        <div className="flex flex-col gap-2 h-screen w-screen items-center justify-center" style={{
+        <div className="flex flex-col gap-2 h-full py-5 w-screen items-center justify-center" style={{
             backgroundImage: 'url(/imgs/background-link-tree.png)',
             backgroundSize: 'cover',
             backgroundPosition: 'center',
@@ -14,7 +14,7 @@ export default function LinkTree() {
             minHeight: '100vh',
             width: '100%'
         }}>
-            <div className="bg-black border-2 rounded-3xl border-[#303030] md:w-fit w-[90%] shadow-md h-[80%] text-white overflow-hidden">
+            <div className="bg-black border-2 rounded-3xl border-[#303030] md:w-fit w-[90%] shadow-md hfull overflow-hidden text-white flex flex-col pb-5">
                 <div className="h-24 border-b-2 border-[#303030]" style={{
                     backgroundImage: 'url(/imgs/header-link-tree.jpg)',
                     backgroundSize: 'cover',
@@ -35,8 +35,8 @@ export default function LinkTree() {
                     </div>
 
                     <nav className="w-full flex justify-between text-[#454242] px-20 py-8">
-                        <a className="hover:text-green-700 transition-colors duration-150" href="https://www.instagram.com/aristoteles269/"><Linkedin /></a>
-                        <a className="hover:text-green-700 transition-colors duration-150" href="https://linkedin.com/in/aristóteles-alves-ab8089226"><Instagram /></a>
+                        <a className="hover:text-green-700 transition-colors duration-150" href="https://linkedin.com/in/aristóteles-alves-ab8089226"><Linkedin /></a>
+                        <a className="hover:text-green-700 transition-colors duration-150" href="https://www.instagram.com/aristoteles269/"><Instagram /></a>
                         <a className="hover:text-green-700 transition-colors duration-150" href="https://api.whatsapp.com/send?phone=5587981152947&text=Olá, tudo bem ?"><Phone /></a>
                         <a className="hover:text-green-700 transition-colors duration-150" href="mailto:aristotelesalves39@gmail.com"><Mail /></a>
                     </nav>
@@ -50,22 +50,22 @@ export default function LinkTree() {
                         </a>
 
                         <a className="w-[90%] flex gap-3 bg-[#191919] px-2 py-4 p-2 border-2 rounded-2xl border-[#303030] hover:border-green-700 transition-colors duration-500" href="https://github.com/AristotelesAlves">
-                            <BriefcaseBusinessIcon className="text-[#454242]" />
+                            <GithubIcon className="text-[#454242]" />
                             <span>
                                 {t.openSource}
                             </span>
                         </a>
 
-                        <div className="w-[90%] flex flex-col h-full gap-3 bg-[#191919] px-2 py-4 p-2 border-2 rounded-2xl border-[#303030] hover:border-green-700 transition-colors duration-500">
+                        <div className="w-[90%] flex flex-col h-full gap-3 bg-[#191919] p-2 border-2 rounded-2xl border-[#303030] hover:border-green-700 transition-colors duration-500">
                             <h2>
                                 {t.lastProject}
                             </h2>
-                            <img className="w-full h-[180px] border-[#303030] hover:border-green-700 border-2 rounded-xl object-cover object-[center_0%]" src="/imgs/salada.png" alt="" />
+                            <img className="w-full h-full border-[#303030] hover:border-green-700 border-2 rounded-xl object-cover object-[center_0%]" src="/imgs/salada.png" alt="" />
                         </div>
                     </nav>
                 </div>
             </div>
-            <span className="text-white italic text-sm">
+            <span className="text-[#CCCCCC]  italic text-sm font-light mt-4">
                 {t.copyright}
             </span>
         </div>
